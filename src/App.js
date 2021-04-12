b94f6aff04cfed68d895074a21aa38152a2634e6
LokiDieKatze <sarah.marques.fr@gmail.com>
2021-04-12 15:15:41 +0200

    push couleurs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import GradientsList from "./components/GradientsList"
 function App() {
   return (
     <div>
-      <h1 className="text-center my-4">Alyra Gradients</h1>
       <main className="container">
         <GradientsList />
       </main>
--- src/components/GradientsList.js
+++ src/components/GradientsList.js
@@ -22,6 +22,111 @@ const GradientsList = () => {
         colorEnd="rgb(63, 43, 150)"
         name="Slight Ocean View"
       />
+      <Gradient
+        colorStart="rgb(21, 153, 87)"
+        colorEnd="rgb(21, 87, 153)"
+        name="Crystal Clear"
+      />
+      <Gradient
+        colorStart="rgb(238, 156, 167)"
+        colorEnd="rgb(255, 221, 225)"
+        name="Piggy Pink"
+      />
+      <Gradient
+        colorStart="rgb(253, 200, 48)"
+        colorEnd="rgb(243, 115, 53)"
+        name="Citrus Peel"
+      />
+      <Gradient
+        colorStart="rgb(238, 9, 121)"
+        colorEnd="rgb(255, 106, 0)"
+        name="Ibiza Sunset"
+      />
+      <Gradient
+        colorStart="rgb(0, 195, 255)"
+        colorEnd="rgb(255, 255, 28)"
+        name="Fun Fun"
+      />
+      <Gradient
+        colorStart="rgb(255, 0, 204)"
+        colorEnd="rgb(51, 51, 153)"
+        name="Cosmic Fusion"
+      />
+      <Gradient
+        colorStart="rgb(255, 126, 95)"
+        colorEnd="rgb(254, 180, 123)"
+        name="Sunrise"
+      />
+      <Gradient
+        colorStart="rgb(252, 234, 187)"
+        colorEnd="rgb(248, 181, 0)"
+        name="Sun on the Horizon"
+      />
+      <Gradient
+        colorStart="rgb(11, 72, 107)"
+        colorEnd="rgb(245, 98, 23)"
+        name="Sunset"
+      />
+      <Gradient
+        colorStart="rgb(251, 211, 233)"
+        colorEnd="rgb(187, 55, 125)"
+        name="Cherryblossoms"
+      />
+      <Gradient
+        colorStart="rgb(30, 60, 114)"
+        colorEnd="rgb(42, 82, 152)"
+        name="Joomla"
+      />
+      <Gradient
+        colorStart="rgb(118, 184, 82)"
+        colorEnd="rgb(141, 194, 111)"
+        name="Little Leaf"
+      />
+      <Gradient
+        colorStart="rgb(82, 82, 82)"
+        colorEnd="rgb(61, 114, 180)"
+        name="Curiosity blue"
+      />
+      <Gradient
+        colorStart="rgb(100, 65, 165)"
+        colorEnd="rgb(42, 8, 69)"
+        name="Twitch"
+      />
+      <Gradient
+        colorStart="rgb(222, 97, 97)"
+        colorEnd="rgb(38, 87, 235)"
+        name="Nepal"
+      />
+      <Gradient
+        colorStart="rgb(255, 78, 80)"
+        colorEnd="rgb(249, 212, 35)"
+        name="Dance To Forget"
+      />
+      <Gradient
+        colorStart="rgb(218, 226, 248)"
+        colorEnd="rgb(214, 164, 164)"
+        name="Moonrise"
+      />
+      <Gradient
+        colorStart="rgb(229, 45, 39)"
+        colorEnd="rgb(179, 18, 23)"
+        name="Youtube"
+      />
+      <Gradient
+        colorStart="rgb(2, 170, 176)"
+        colorEnd="rgb(0, 205, 172)"
+        name="Green Beach"
+      />
+      <Gradient
+        colorStart="rgb(204, 43, 94)"
+        colorEnd="rgb(117, 58, 136)"
+        name="Purple Love"
+      />
+      <Gradient
+        colorStart="rgb(22, 160, 133)"
+        colorEnd="rgb(244, 208, 63)"
+        name="Harmonic Energy"
+      />
     </ul>
   )
 }
